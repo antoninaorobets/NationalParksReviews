75aba127d420104cbeff90b436ba800ffea1390f
antoninaorobets <pani.antonina.don@gmail.com>
2022-06-19 14:11:54 -0400

    React setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
 
     def hello_world
         session[:count] = (session[:count] || 0) + 1
-        render json: { count: session }
+        render json: { count: 1 }
       end
 end
