76bbe1fdb709d2ae1c229a7dd5c3df2d8bc76321
antoninaorobets <pani.antonina.don@gmail.com>
2022-06-23 11:01:33 -0400

    add parks car and parks info

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  resources :parks, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get '/hello', to: 'application#hello_world'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+def api_data 
+    url = URI("https://jonahtaylor-national-park-service-v1.p.rapidapi.com/parks?limit=50")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["X-Api-Key"] = '9nzLdIEabRYheVamhU9emwHWyYsShAp7PkFigs9r'
+    request["X-RapidAPI-Key"] = '03cbacb2d6msh147d7e91d53a89fp189018jsn4c2e0d1cd811'
+    request["X-RapidAPI-Host"] = 'jonahtaylor-national-park-service-v1.p.rapidapi.com'
+
+    response = http.request(request)
+    data = JSON.parse(response.read_body)["data"]
+end
+data = api_data
+
+data.each do |park|
+    a = park["addresses"][0]
+    a["type"] = ''
+    adress = a.reduce('') {|memo, (key, val)| memo + " " +val}
+   p = Park.create(
+     api_id: park["id"],
+     name:  park["name"],
+     full_name: park["fullName"],
+     park_code: park["parkCode"],
+     description: park["description"],
+     states: park["states"],
+     directions_info: park["directionsInfo"],
+    #  operating_hours: park["operatingHours"][0]["description"],
+     address:  adress,
+     weather_info: park["weatherInfo"],
+     designation: park["designation"],
+     url: park["url"] 
+  )
+  park["images"].each do |image|
+     Image.create(
+         title:  image["title"],
+         alt_text: image["altText"],
+         caption: image["caption"],
+        url: image["url"],
+        park_id: p.id
+     )
+  end
+end
+
+puts "done"
+# puts obj["data"][0].keys
+# puts obj["data"][0]["images"][1]
+#  puts Park.last
+#  puts Image.all
