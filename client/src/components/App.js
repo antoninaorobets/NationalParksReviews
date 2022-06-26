6e4decc9a9ff3933830fc4b2cba3c5f515ecc673
antoninaorobets <pani.antonina.don@gmail.com>
2022-06-26 09:41:06 -0400

    add bootstrap

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,33 +1,33 @@
 
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect, useContext } from "react";
+import {  Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm"
 import TopBar from "./TopBar";
 import ParksList from "./ParksList"
 import ParkInfo from "./ParkInfo";
-import { UserProvider } from "../context/user"
+import { UserContext} from "../context/user"
 import MyParks from "./MyParks";
 import Test from "./Test";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const {isLoggedIn} = useContext(UserContext)
   
   return (
-    <BrowserRouter>
       <div className="App">
-        <Test />
-        <UserProvider>
           <TopBar />
           <Routes>
             <Route path="/login" element={< LoginForm />} />
             <Route path="/signup" element={< SignUpForm />} />
             <Route path="/parks/:id" element={<ParkInfo />} />
-            <Route path="/parks/my_parks" element={<MyParks />} />
+            <Route path="/parks/my_parks" element={ (isLoggedIn) 
+              ? <MyParks />
+              : <Navigate replace to="/parks"/>
+              } />
             <Route path="/parks" element={<ParksList />} />
           </Routes>
-        </UserProvider>
       </div>
-    </BrowserRouter>
   );
 }
 
