5ceb67b61ee412c906b29d52443ebf24c93e6261
antoninaorobets <pani.antonina.don@gmail.com>
2022-06-28 11:16:19 -0400

    add styles for com ment

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,31 +1,33 @@
 import React, { useState, useContext } from 'react'
 import { UserContext } from '../context/user';
-import { ButtonGroup, Button, Form } from 'react-bootstrap';
+import { Card, Button, Form, Stack } from 'react-bootstrap';
+import { format } from 'fecha'
 
-function Comment({ comment, handleDelete, handleUpdate}) {
-    const [ isEditMode, setIsEditMode ] = useState(false)
-    const { user, isLoggedIn } = useContext(UserContext)
-    const [text, setText] =useState(comment.text)
 
-    const saveComment =(e)=>{
+function Comment({ comment, handleDelete, handleUpdate }) {
+    const [isEditMode, setIsEditMode] = useState(false)
+    const { user} = useContext(UserContext)
+    const [text, setText] = useState(comment.text)
+
+    const saveComment = (e) => {
         e.preventDefault()
         console.log('save')
-        fetch(`/parks/${comment.park_id}/comments/${comment.id}`,{
+        fetch(`/parks/${comment.park_id}/comments/${comment.id}`, {
             method: "PATCH",
-            headers:  {"Content-Type": "application/json" },
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 "text": text
             })
-        }).then(r=>{
+        }).then(r => {
             if (r.ok) {
-               r.json().then(comment => {
-                handleUpdate(comment)
-                setIsEditMode(false)
-               })
-                
+                r.json().then(comment => {
+                    handleUpdate(comment)
+                    setIsEditMode(false)
+                })
+
             }
-            else {}
-        })       
+            else { }
+        })
     }
     const onDelete = (e) => {
         fetch(`/parks/${comment.park_id}/comments/${comment.id}`, {
@@ -39,31 +41,40 @@ function Comment({ comment, handleDelete, handleUpdate}) {
             }
         })
     }
-    
-    const commentRead = <Form.Control type="text" placeholder={text} disabled readOnly/>
-   
-    const commentEdit = 
+
+    const commentRead = <Form.Control type="text" placeholder={text} disabled readOnly />
+
+    const commentEdit =
         <Form onSubmit={saveComment}>
-            <Form.Control type="text" value={text} onChange={(e)=> setText(e.target.value)} />
+            <Form.Control type="text" value={text} onChange={(e) => setText(e.target.value)} />
             <Button type="submit">Save</Button>
-        </Form> 
+        </Form>
+
+    const commentDate = format(new Date(comment.created_at.substring(0, 10)), 'dddd MMMM Do')
 
     return (
-        <div>
-            {comment.user.username}
-             {(isEditMode)
-            ? commentEdit
-            : commentRead
-            }
-            {(isLoggedIn && user.id === comment.user.id && !isEditMode)
-                ?  <Button variant="secondary" onClick={()=>{setIsEditMode(true)}}>Edit</Button>
-                : null}
 
-            {(isLoggedIn && user.id === comment.user.id)
-                ? <Button variant="secondary" onClick={onDelete}>Delete</Button>
-                : null}
+        <Card border="secondary" >
+            <Card.Header as="h5"> {comment.user.username} </Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    {(isEditMode)
+                        ? commentEdit
+                        : commentRead
+                    }
+                    <cite title="posted"> {commentDate} </cite>
+                </Card.Text>
+                <Stack direction="horizontal" gap={3}>
+                    {(user.id === comment.user.id && !isEditMode)
+                        ? <Button variant="secondary" onClick={() => { setIsEditMode(true) }}>Edit</Button>
+                        : null}
+                    {(user.id === comment.user.id)
+                        ? <Button variant="secondary" onClick={onDelete}>Delete</Button>
+                        : null}
+                </Stack>
+            </Card.Body>
+        </Card>
 
-        </div>
     )
 }
 
