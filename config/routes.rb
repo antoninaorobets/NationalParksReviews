d39bbfb25744f9b1dd0f8c5d89988049942404c6
antoninaorobets <pani.antonina.don@gmail.com>
2022-06-28 23:04:22 -0400

    Add fallback controller to enable SPA

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,8 @@ Rails.application.routes.draw do
   delete '/api/logout', to: 'sessions#destroy'
 
   get '/api/hello', to: 'application#hello_world'
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
